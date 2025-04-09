08355b2098d57a85b6170148d68d248a1369ac58
AthaliahB <lr.maabernardo@mmdc.mcl.edu.ph>
2025-04-09 21:56:09 +0800

    Add files via upload

---
+++ PayrollSystemForMotorPH.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cp1.payrollsystemformotorph;
+
+//Imports
+
+
+/**
+ *
+ * @author athaliah
+ */
+public class PayrollSystemForMotorPH {
+
+// Final Payroll System 
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
